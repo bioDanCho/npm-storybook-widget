6f4cfbf1d04217ca217b2e5981740d561b99c40a
bioDanCho <daniel.cho@thebioagency.com>
2023-08-17 19:52:24 -0400

    remove unused styles

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,4 @@
 import React from 'react';
-import './styles.scss';
 export type AppProps = {
     projectSource: string;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './styles.scss';
 import FabWidget from './components/organisms/FabWidget';
 
 import { createClient } from '@supabase/supabase-js';
